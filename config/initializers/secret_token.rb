2e7ca8a7d95be6765173735752de39f15dcdd668
Graham Lewis <gclikkec@mac.com>
2012-11-10 10:09:16 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BengwallerCom::Application.config.secret_token = 'e35eca23da7e73d882b17acf98996799cd59a80c6f8f8cf4697f963cd6d28532a38dec09f52a8e0fde009b405ef9b9d733b1a17bf60677f27e249d9e237454ae'
